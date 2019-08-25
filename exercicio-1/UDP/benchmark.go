861df405a31ee73f0ad077d187f93fa419c2fe2c
Caio Gomes <caiogomes34@gmail.com>
2019-08-25 16:37:07 -0300

    feat: benchmark udp

--- exercicio-1/UDP/benchmark.go
+++ exercicio-1/UDP/benchmark.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
+	lst := make([]time.Duration, 0, 0)
+
 	addr, err := net.ResolveUDPAddr("udp", "localhost:6000")
 	if err != nil {
 		fmt.Println(err)
@@ -20,19 +23,22 @@ func main() {
 		return
 	}
 
-	for i = 0; i < 10000; i += 1 {
-		fmt.Print("Text to send: ")
-		reader := bufio.NewReader(os.Stdin)
+	text := "seja muito bem vindo a vida real\n"
 
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	for i := 0; i < 10000; i += 1 {
+		// fmt.Print("Text to send: ")
+		// reader := bufio.NewReader(os.Stdin)
+
+		// text, err := reader.ReadString('\n')
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
 
 		message := []byte(text)
 
 		// send
+		tm1 := time.Now()
 		_, err = conn.Write(message)
 		if err != nil {
 			fmt.Println(err)
@@ -47,6 +53,25 @@ func main() {
 			return
 		}
 
+		tm2 := time.Now()
+		diff := tm2.Sub(tm1)
+		lst = append(lst, diff)
+		fmt.Println("delay time: ", diff)
 		fmt.Println("Message from server: ", string(buffer[:n]))
+	}
+
+
+	// put in a csv
+	f, err := os.Create("benchmark_udp_1.csv")
+	if err != nil {
+        panic(err)
+    }
+	defer f.Close()
+
+	// fmt.Println(lst)
+	for idx, tm := range lst {
+		dt := fmt.Sprintf("%d,%s\n", idx, tm.String())
+		// fmt.Println(dt)
+		f.Write([]byte(dt))
 	}
 }
